5c9212b6535a2985bf325150d59cbeefbaa69c31
fvd2 <fmvd1931@outlook.com>
2021-11-16 14:56:01 +0100

    changed customer endpoint return type to ResponseEntity

--- src/main/java/com/fvd2/customerdetailsservice/controller/CustomerController.java
+++ src/main/java/com/fvd2/customerdetailsservice/controller/CustomerController.java
@@ -4,14 +4,16 @@ import com.fvd2.customerdetailsservice.model.Customer;
 import com.fvd2.customerdetailsservice.services.CustomerService;
 import lombok.AllArgsConstructor;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/customers")
+@RequestMapping("v1/customers")
 @CrossOrigin
 @AllArgsConstructor
 public class CustomerController {
@@ -19,13 +21,13 @@ public class CustomerController {
     private CustomerService customerService;
 
     @GetMapping
-    public List<Customer> getCustomers() {
-        return customerService.getCustomers();
+    public ResponseEntity<List<Customer>> getCustomers() {
+        return new ResponseEntity<>(customerService.getCustomers(), HttpStatus.OK);
     }
 
     @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
-    public Customer addCustomer(@RequestBody @Valid Customer customer) {
-        return customerService.addCustomer(customer);
+    public ResponseEntity<Customer> addCustomer(@RequestBody @Valid Customer customer) {
+        return new ResponseEntity<>(customerService.addCustomer(customer), HttpStatus.CREATED);
     }
 }
 
